b17e2cb5c84183ab29443c1dd13f8c0904fd9da8
garima-ops <garimas028@gmail.com>
2025-07-01 02:24:39 +0530

    Add files via upload

---
+++ identify number of characters.py
@@ -0,0 +1,7 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> user_input = input("name of country :")
+name of country : India
+>>> character_count = len(user_input)
+>>> print ("number of characaters is :" , character_count)
+number of characaters is : 6
